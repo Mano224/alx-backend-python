b73531763e3ab7bbde7d4b6ac4a537171749aceb
Mano224 <Mano224@example.com>
2024-10-12 21:42:21 +0300

    d

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -2,4 +2,4 @@
 #Write a type-annotated function concat that takes a string str1 and a string str2 as arguments and returns a concatenated string
 def concat(str1: str, str2: str) -> str:
     '''Write a type-annotated function concat that takes a string str1 and a string str2 as arguments and returns a concatenated string'''
-    return str1 + str2
+    return (str1 + str2)
