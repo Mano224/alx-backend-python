5cf52140cffd75b77b9ab66c1ff420f559d348fa
Mano224 <Mano224@example.com>
2024-10-12 22:45:23 +0300

    e

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -2,9 +2,10 @@
 '''
 type-annotated function
 '''
-
-
 from typing import List
 
+
 def sum_list(input_list: List[float]) -> float:
+    '''takes list of float and reture summed value 
+    '''
     return float(sum(input_list))
